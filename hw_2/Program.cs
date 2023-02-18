17e5b78854ff84cc8f6b842c767234bfe40ee036
Nazik <nazikbaktygulova77@gmail.com>
2023-02-18 17:42:41 +0600

    Написана программа для задачи №2 в папке hw_2

---
+++ hw_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int GetThird(int val) {
+    return val % 10;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a > 99 && a < 1000) {
+    Console.WriteLine(GetThird(a));
+}
+else {
+    Console.WriteLine("Третьей цифры нет!");
+}

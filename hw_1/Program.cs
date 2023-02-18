20069b0222e22ee72ad55df32e1025f75187efb0
Nazik <nazikbaktygulova77@gmail.com>
2023-02-18 17:35:46 +0600

    Написана программа для задачи №1 в папке hw_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+int GetSecond(int val) {
+    return (val%100)/10;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSecond(a));
